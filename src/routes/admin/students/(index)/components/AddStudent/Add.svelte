10cb7713076f236ca5b964bafb58744e29e8f599
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-12 01:24:08 +0800

    add dynamic add student form

--- src/routes/admin/students/(index)/components/AddStudent/Add.svelte
+++ src/routes/admin/students/(index)/components/AddStudent/Add.svelte
@@ -10,8 +10,9 @@
   import { zodClient } from 'sveltekit-superforms/adapters';
   import { Label } from '$lib/components/ui/label/index';
   import { flip } from 'svelte/animate';
-  import { cubicInOut } from 'svelte/easing';
   import { fly } from 'svelte/transition';
+  import SelectPicker from '$lib/components/general/SelectPicker.svelte';
+  import Combobox from '$lib/components/general/Combobox.svelte';
 
   interface Props {
     addStudentForm: SuperValidated<Infer<AddStudentSchema>>;
@@ -19,7 +20,7 @@
 
   const { addStudentForm }: Props = $props();
 
-  let open = $state(true);
+  let open = $state(false);
 
   const form = superForm(addStudentForm, {
     validators: zodClient(addStudentSchema)
@@ -38,12 +39,14 @@
 
   let subjects = $state([
     {
+      id: crypto.randomUUID(),
       name: '',
-      schedule: [
+      schedules: [
         {
+          id: crypto.randomUUID(),
           startTime: '',
           endTime: '',
-          days: ''
+          day: ''
         }
       ]
     }
@@ -55,7 +58,10 @@
 <AlertDialog.Root bind:open>
   <AlertDialog.Content class="max-h-[90dvh] max-w-[1200px] p-0">
     <button
-      onclick={() => (open = false)}
+      onclick={() => {
+        form.reset();
+        open = false;
+      }}
       class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
     >
       <X class="size-4" />
@@ -153,86 +159,254 @@
           </section>
 
           <section>
-            <Form.Field {form} name="gender">
-              <Form.Control>
-                {#snippet children({ props })}
-                  <Form.Label>Gender</Form.Label>
-                  <Input {...props} bind:value={$formData.gender} placeholder="Enter Gender" />
-                {/snippet}
-              </Form.Control>
-              <Form.FieldErrors />
-            </Form.Field>
+            <div class="sticky top-0">
+              <Form.Field {form} name="gender">
+                <Form.Control>
+                  {#snippet children({ props })}
+                    <Form.Label>Gender</Form.Label>
+                    <Input {...props} bind:value={$formData.gender} placeholder="Enter Gender" />
+                  {/snippet}
+                </Form.Control>
+                <Form.FieldErrors />
+              </Form.Field>
 
-            <Form.Field {form} name="yearLevel">
-              <Form.Control>
-                {#snippet children({ props })}
-                  <Form.Label>Year Level</Form.Label>
-                  <Input
-                    {...props}
-                    bind:value={$formData.yearLevel}
-                    placeholder="Enter Year Level"
-                  />
-                {/snippet}
-              </Form.Control>
-              <Form.FieldErrors />
-            </Form.Field>
-
-            <div class="mt-[16px] flex flex-col gap-[16px]">
-              {#each sections as section, i (section)}
-                <div
-                  class="flex w-full flex-col gap-[10px]"
-                  transition:fly={{ x: 100, duration: 200 }}
-                >
-                  <Label for={`${section.id}-section`}>Section {i + 1}</Label>
-                  <div class="relative" bind:this={divElement}>
+              <Form.Field {form} name="yearLevel">
+                <Form.Control>
+                  {#snippet children({ props })}
+                    <Form.Label>Year Level</Form.Label>
                     <Input
-                      type="text"
-                      id={`${section.id}-section`}
-                      class="pr-[45px]"
-                      placeholder={`Enter section ${i + 1}`}
-                      bind:value={section.name}
+                      {...props}
+                      bind:value={$formData.yearLevel}
+                      placeholder="Enter Year Level"
                     />
-                    {#if sections.length > 1}
-                      <Button
-                        size="sm"
-                        onclick={() => sections.splice(i, 1)}
-                        class="absolute right-0 top-1/2 -translate-y-1/2"
-                      >
-                        <X class="size-4" />
-                      </Button>
-                    {/if}
+                  {/snippet}
+                </Form.Control>
+                <Form.FieldErrors />
+              </Form.Field>
+
+              <div class="mt-[16px] flex flex-col gap-[16px]">
+                {#each sections as section, i (section)}
+                  <div
+                    class="flex w-full flex-col gap-[10px]"
+                    transition:fly={{ x: 100, duration: 200 }}
+                  >
+                    <Label for={`${section.id}-section`}>Section {i + 1}</Label>
+                    <div class="relative" bind:this={divElement}>
+                      <Input
+                        type="text"
+                        id={`${section.id}-section`}
+                        class="pr-[45px]"
+                        placeholder={`Enter section ${i + 1}`}
+                        bind:value={section.name}
+                      />
+                      {#if sections.length > 1}
+                        <Button
+                          size="sm"
+                          onclick={() => sections.splice(i, 1)}
+                          class="absolute right-0 top-1/2 -translate-y-1/2"
+                        >
+                          <X class="size-4" />
+                        </Button>
+                      {/if}
+                    </div>
                   </div>
-                </div>
-              {/each}
-              <div class="">
-                <Button
-                  onclick={() => {
-                    sections.push({ id: crypto.randomUUID(), name: '' });
-                    setTimeout(() => {
-                      divElement?.scrollIntoView({ behavior: 'smooth' });
-                    }, 200);
-                  }}
-                >
-                  More Section
-                </Button>
-                {#if sections.length > 3}
+                {/each}
+                <div class="">
                   <Button
-                    size="sm"
-                    variant="outline"
                     onclick={() => {
-                      sections = sections.slice(0, 1);
+                      sections.push({ id: crypto.randomUUID(), name: '' });
+                      setTimeout(() => {
+                        divElement?.scrollIntoView({ behavior: 'smooth' });
+                      }, 200);
                     }}
                   >
-                    Clear
+                    More Section
                   </Button>
-                {/if}
+                  {#if sections.length > 3}
+                    <Button
+                      size="sm"
+                      variant="outline"
+                      onclick={() => {
+                        sections = sections.slice(0, 1);
+                      }}
+                    >
+                      Clear
+                    </Button>
+                  {/if}
+                </div>
               </div>
             </div>
           </section>
 
-          <section></section>
+          <section class="flex flex-col gap-2.5">
+            <div class="flex flex-col gap-2.5">
+              {#each subjects as subject, i (subject)}
+                <div
+                  class="relative flex w-full flex-col gap-[10px] border-b-2"
+                  transition:fly={{ x: 100, duration: 200 }}
+                  bind:this={divElement}
+                >
+                  {#if subjects.length > 1}
+                    <Button
+                      size="sm"
+                      onclick={() => subjects.splice(i, 1)}
+                      class="absolute right-0 top-5 z-10 -translate-y-1/2"
+                    >
+                      <X class="size-4" />
+                    </Button>
+                  {/if}
+                  <Label for={`${subject.id}-subject`}>Subject</Label>
+                  <div class="relative">
+                    <Combobox
+                      selections={[
+                        { label: 'Math', value: 'math' },
+                        { label: 'Science', value: 'science' },
+                        { label: 'English', value: 'english' },
+                        { label: 'History', value: 'history' }
+                      ]}
+                      bind:selected={subject.name}
+                      placeholder="Select subject"
+                      name="Select Subject"
+                    />
+
+                    <ScrollArea class="h-[30dvh]">
+                      <div class="flex flex-col gap-2.5 p-2">
+                        {#each subject.schedules as schedule, j (schedule)}
+                          <section
+                            class="relative flex flex-col gap-[10px] rounded-lg border-2 p-2"
+                            bind:this={divElement}
+                          >
+                            {#if subject.schedules.length > 1}
+                              <Button
+                                size="sm"
+                                onclick={() => subjects[i].schedules.splice(j, 1)}
+                                class="absolute right-0 top-5 z-10 -translate-y-1/2"
+                              >
+                                <X class="size-4" />
+                              </Button>
+                            {/if}
+                            <div class="flex flex-col gap-[10px]">
+                              <Label for={`${schedule.id}-${j}-day`}>Day</Label>
+                              <div class="relative">
+                                <Combobox
+                                  selections={[
+                                    { label: 'Math', value: 'math' },
+                                    { label: 'Science', value: 'science' },
+                                    { label: 'English', value: 'english' },
+                                    { label: 'History', value: 'history' }
+                                  ]}
+                                  bind:selected={schedule.day}
+                                  placeholder="Select Day"
+                                  name="Select Day"
+                                />
+                              </div>
+                            </div>
+
+                            <div class="grid grid-cols-2 gap-[10px]">
+                              <div class="flex flex-col gap-[10px]">
+                                <Label for={`${schedule.id}-${j}-startTime`}>Start Time</Label>
+                                <div class="relative">
+                                  <Combobox
+                                    selections={[
+                                      { label: 'Math', value: 'math' },
+                                      { label: 'Science', value: 'science' },
+                                      { label: 'English', value: 'english' },
+                                      { label: 'History', value: 'history' }
+                                    ]}
+                                    bind:selected={schedule.startTime}
+                                    placeholder="Select Start Time"
+                                    name="Select Start Time"
+                                  />
+                                </div>
+                              </div>
+
+                              <div class="flex flex-col gap-[10px]">
+                                <Label for={`${schedule.id}-${j}-endTime`}>End Time</Label>
+                                <div class="relative">
+                                  <Combobox
+                                    selections={[
+                                      { label: 'Math', value: 'math' },
+                                      { label: 'Science', value: 'science' },
+                                      { label: 'English', value: 'english' },
+                                      { label: 'History', value: 'history' }
+                                    ]}
+                                    bind:selected={schedule.endTime}
+                                    placeholder="Select End Time"
+                                    name="Select End Time"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                          </section>
+                        {/each}
+
+                        <div class="">
+                          <Button
+                            size="sm"
+                            onclick={() => {
+                              subjects[i].schedules.push({
+                                id: crypto.randomUUID(),
+                                day: '',
+                                startTime: '',
+                                endTime: ''
+                              });
+                              setTimeout(() => {
+                                divElement?.scrollIntoView({ behavior: 'smooth' });
+                              }, 200);
+                            }}
+                          >
+                            More Schedule
+                          </Button>
+
+                          {#if subjects[i].schedules.length > 3}
+                            <Button
+                              size="sm"
+                              variant="outline"
+                              onclick={() =>
+                                (subjects[i].schedules = subjects[i].schedules.slice(0, 1))}
+                            >
+                              Clear
+                            </Button>
+                          {/if}
+                        </div>
+                      </div>
+                    </ScrollArea>
+                  </div>
+                </div>
+              {/each}
+            </div>
+
+            <div class="z-10">
+              <Button
+                size="sm"
+                onclick={() => {
+                  subjects.push({
+                    id: crypto.randomUUID(),
+                    name: '',
+                    schedules: [{ id: crypto.randomUUID(), day: '', startTime: '', endTime: '' }]
+                  });
+                  setTimeout(() => {
+                    divElement?.scrollIntoView({ behavior: 'smooth' });
+                  }, 200);
+                }}
+              >
+                More Subject
+              </Button>
+              {#if subjects.length > 3}
+                <Button
+                  size="sm"
+                  variant="outline"
+                  onclick={() => (subjects = subjects.slice(0, 1))}
+                >
+                  Clear
+                </Button>
+              {/if}
+            </div>
+          </section>
         </div>
-        <Form.Button>Submit</Form.Button>
+        <div class="absolute bottom-6 right-6">
+          <Form.Button>Submit</Form.Button>
+        </div>
       </form>
     </ScrollArea>
   </AlertDialog.Content>
