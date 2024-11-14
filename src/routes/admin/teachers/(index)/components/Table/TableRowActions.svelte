81b00517ec975cec9ee0f32a913e971043450400
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-15 01:25:37 +0800

    update teachers page UI

---
+++ src/routes/admin/teachers/(index)/components/Table/TableRowActions.svelte
@@ -0,0 +1,53 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import Ellipsis from 'lucide-svelte/icons/ellipsis';
+  import UserPen from 'lucide-svelte/icons/user-pen';
+  import UserRoundMinus from 'lucide-svelte/icons/user-round-minus';
+  import type { Row } from '@tanstack/table-core';
+  import { userSchema } from './schema';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+  import { Button } from '$lib/components/ui/button';
+  import { tableState } from '../tableState.svelte';
+
+  interface Props {
+    row: Row<TData>;
+  }
+
+  let { row }: Props = $props();
+
+  const user = userSchema.parse(row.original);
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button {...props} variant="ghost" class="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+        <Ellipsis />
+        <span class="sr-only">Open Menu</span>
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content class="" align="start">
+    <DropdownMenu.Item
+      onclick={() => {
+        tableState.setUpdateState(true);
+        //set active row
+      }}
+    >
+      <UserPen />
+      Update
+    </DropdownMenu.Item>
+    <DropdownMenu.Item
+      onclick={() => {
+        tableState.setDeleteState(true);
+        //set active row
+      }}
+    >
+      <UserRoundMinus />
+      Delete
+    </DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
---
+++ src/routes/admin/teachers/(index)/components/Table/TableRowSubjects.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import type { Row } from '@tanstack/table-core';
+  import { userSchema } from './schema';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+  import { Button } from '$lib/components/ui/button';
+  import Expand from 'lucide-svelte/icons/expand';
+  let { row }: { row: Row<TData> } = $props();
+
+  const user = userSchema.parse(row.original);
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button {...props} variant="ghost" class="flex h-8 p-0 px-2 data-[state=open]:bg-muted">
+        <Expand class="size-4" />
+        <span>View Subjects</span>
+        <span class="sr-only">Open Subjects</span>
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content class="w-fit" align="end">
+    {#each user.subjects as subject}
+      <DropdownMenu.Sub>
+        <DropdownMenu.SubTrigger>
+          <span>{subject.name}</span>
+        </DropdownMenu.SubTrigger>
+        <DropdownMenu.SubContent>
+          {#each subject.schedules as schedule}
+            <DropdownMenu.Label class="text-sm font-medium">
+              <span>{schedule.day} - {schedule.startTime} - {schedule.endTime}</span>
+            </DropdownMenu.Label>
+          {/each}
+        </DropdownMenu.SubContent>
+      </DropdownMenu.Sub>
+    {/each}
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
