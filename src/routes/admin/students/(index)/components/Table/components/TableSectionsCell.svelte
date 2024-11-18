609dc18e7826901621c5939a6f8b7cb9311fdb63
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-18 20:39:08 +0800

    update icons

--- src/routes/admin/students/(index)/components/Table/components/TableSectionsCell.svelte
+++ src/routes/admin/students/(index)/components/Table/components/TableSectionsCell.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <script lang="ts" generics="TData">
-  import Ellipsis from 'lucide-svelte/icons/ellipsis';
-  import FileMinus from 'lucide-svelte/icons/file-minus';
-  import Pen from 'lucide-svelte/icons/pen';
+  import Expand from 'lucide-svelte/icons/expand';
   import type { Row } from '@tanstack/table-core';
   import { type StudentPageSchema } from '../data/schemas';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
@@ -14,7 +12,12 @@
 </script>
 
 <DropdownMenu.Root>
-  <DropdownMenu.Trigger class="underline">View</DropdownMenu.Trigger>
+  <DropdownMenu.Trigger
+    class="flex items-center gap-1.5 transition-all duration-150 hover:underline"
+  >
+    <Expand class="size-4" />
+    View
+  </DropdownMenu.Trigger>
   <DropdownMenu.Content class="max-w-[300px] p-2">
     <section class="flex flex-wrap gap-1.5">
       {#each row.original.sections as section}
--- src/routes/admin/students/(index)/components/Table/components/TableSubjectsCell.svelte
+++ src/routes/admin/students/(index)/components/Table/components/TableSubjectsCell.svelte
@@ -3,18 +3,21 @@
 </script>
 
 <script lang="ts" generics="TData">
-  import Ellipsis from 'lucide-svelte/icons/ellipsis';
-  import FileMinus from 'lucide-svelte/icons/file-minus';
   import Pen from 'lucide-svelte/icons/pen';
   import type { Row } from '@tanstack/table-core';
   import { type StudentPageSchema } from '../data/schemas';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-  import Badge from '$lib/components/ui/badge/badge.svelte';
+  import Expand from 'lucide-svelte/icons/expand';
   let { row }: { row: Row<StudentPageSchema> } = $props();
 </script>
 
 <DropdownMenu.Root>
-  <DropdownMenu.Trigger class="underline">View</DropdownMenu.Trigger>
+  <DropdownMenu.Trigger
+    class="flex items-center gap-1.5 transition-all duration-150 hover:underline"
+  >
+    <Expand class="size-4" />
+    View
+  </DropdownMenu.Trigger>
   <DropdownMenu.Content class="max-w-[300px] p-2">
     <section class="flex flex-wrap gap-1.5">
       {#each row.original.subjects as subject}
