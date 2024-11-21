0a0953958e4bcd0098bd8487169a2c5c4af0466b
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-21 17:49:43 +0800

    update login page

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -6,7 +6,7 @@
   const { data } = $props();
 </script>
 
-<main class="relative grid min-h-screen grid-cols-[3fr_2fr]">
+<!-- <main class="relative grid min-h-screen grid-cols-[3fr_2fr]">
   <enhanced:img
     src="/static/assets/system-bg.png"
     alt="system background"
@@ -32,6 +32,33 @@
           {/if}
         </div>
       </div>
+    </div>
+  </section>
+</main> -->
+
+<main class="grid h-screen grid-cols-[3fr_2fr]">
+  <enhanced:img
+    src="/static/assets/system-bg.png"
+    alt="system background"
+    class="h-full w-full object-cover"
+  />
+
+  <section>
+    <div class="flex flex-col items-center justify-center gap-10 rounded-lg bg-white/20 p-5">
+      <section class="s flex flex-col items-center justify-center gap-2.5">
+        <enhanced:img src="/static/assets/logo.png" alt="logo" class=" w-[200px]" />
+        <span class="text-center text-2xl font-bold text-primary">
+          ITE Department Scheduling System
+        </span>
+
+        <div class="mt-[10dvh]">
+          {#if $page.url.searchParams.get('switch') === 'forgot-password'}
+            <ForgotPwd forgotPwdForm={data.forgotPwdForm} />
+          {:else}
+            <Login loginForm={data.loginForm} />
+          {/if}
+        </div>
+      </section>
     </div>
   </section>
 </main>
---
+++ src/routes/student/(index)/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="">
+  <h1>Student</h1>
+</div>
