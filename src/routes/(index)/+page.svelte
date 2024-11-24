d1f206da28db3c3d78e192de576ace00b630e555
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-24 16:21:18 +0800

    update

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -6,36 +6,6 @@
   const { data } = $props();
 </script>
 
-<!-- <main class="relative grid min-h-screen grid-cols-[3fr_2fr]">
-  <enhanced:img
-    src="/static/assets/system-bg.png"
-    alt="system background"
-    class="absolute bottom-0 left-0 right-0 top-0 h-full w-full object-cover"
-  />
-
-  <section class="relative flex items-center justify-center">
-    <div class="absolute inset-0 bg-black/20"></div>
-    <div class="relative z-10 flex flex-col items-center justify-center gap-10 rounded-lg">
-      <section class="flex flex-col items-center justify-center gap-2.5 text-white">
-        <enhanced:img src="/static/assets/logo.png" alt="logo" class=" w-[200px]" />
-        <span class="text-center text-2xl font-bold">ITE Department Scheduling System</span>
-      </section>
-
-      <div
-        class="flex max-w-[320px] flex-col items-center justify-center gap-2.5 rounded-lg bg-white/60 p-5"
-      >
-        <div class="">
-          {#if $page.url.searchParams.get('switch') === 'forgot-password'}
-            <ForgotPwd forgotPwdForm={data.forgotPwdForm} />
-          {:else}
-            <Login loginForm={data.loginForm} />
-          {/if}
-        </div>
-      </div>
-    </div>
-  </section>
-</main> -->
-
 <main class="grid h-screen grid-cols-[3fr_2fr]">
   <enhanced:img
     src="/static/assets/system-bg.png"
